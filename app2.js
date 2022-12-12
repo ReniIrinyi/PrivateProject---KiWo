d47154577ff79220c206cd114a19a7093301bb51
reniirinyi <reniirinyi@icloud.com>
2022-12-12 11:09:51 +0100

    wrapping up

--- app2.js
+++
@@ -1,7 +0,0 @@
-const imgs = document.querySelectorAll(".gallerie--img");
-imgs.forEach((e) =>
-  e.addEventListener("click", function (e) {
-    imgs.forEach((e) => e.classList.remove("fadein"));
-    e.target.classList.add("fadein");
-  })
-);
