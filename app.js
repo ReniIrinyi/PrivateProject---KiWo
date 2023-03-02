dad4b050c0b0ea88957d679922c925f75824b359
RenataIrinyi <reniirinyi@icloud.com>
2023-03-02 21:18:52 +0100

    adding impressum

--- app.js
+++ app.js
@@ -62,24 +62,6 @@ const observer = new IntersectionObserver(callback, {
 });
 observer.observe(home);
 
-//Reveling Sections
-const allSections = document.querySelectorAll(".section");
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-});
-allSections.forEach((section) => {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
-
 //Slider
 const dots = document.querySelector(".slider-dots");
 const sliderCompontents = document.querySelectorAll(".component-content");
