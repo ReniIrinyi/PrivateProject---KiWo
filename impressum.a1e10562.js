3b8c7a077e93318ce90ad9195723df431e3e6267
RenataIrinyi <reniirinyi@icloud.com>
2023-03-24 19:54:47 +0100

    cleaning the repo

--- impressum.a1e10562.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll(".nav-item"),c=document.querySelector(".menuIcon");e.forEach((e=>e.addEventListener("click",(function(){1==c.checked?c.checked=!1:0==c.checked&&(c.checked=!0)}))));
-//# sourceMappingURL=impressum.a1e10562.js.map
