2b0f2688a6d8dc69f113f722f49d55d89b4379ab
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2025-02-12 23:26:20 +0100

    update

--- kiwo-frontend/src/app/app.module.ts
+++ kiwo-frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,44 +33,37 @@ import { CookiesComponent } from './components/cookies/cookies.component';
 import { CookiesSettingsComponent } from './components/cookies-settings/cookies-settings.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    GalleryComponent,
-    InfosComponent,
-    AboutComponent,
-    FormComponent,
-    FooterComponent,
-    AboutusComponent,
-    RegistrationComponent,
-    LinksComponent,
-    ShareLinksComponent,
-    ContactComponent,
-    ImpressumComponent,
-    MainComponent,
-    CookiesComponent,
-    CookiesSettingsComponent,
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    BrowserAnimationsModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatCardModule,
-    MatDialogModule,
-    MatSnackBarModule,
-  ],
-  providers: [DatePipe,  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        GalleryComponent,
+        InfosComponent,
+        AboutComponent,
+        FormComponent,
+        FooterComponent,
+        AboutusComponent,
+        RegistrationComponent,
+        LinksComponent,
+        ShareLinksComponent,
+        ContactComponent,
+        ImpressumComponent,
+        MainComponent,
+        CookiesComponent,
+        CookiesSettingsComponent,
+    ],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        BrowserAnimationsModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatCardModule,
+        MatDialogModule,
+        MatSnackBarModule], providers: [DatePipe, provideHttpClient(withInterceptorsFromDi()),] })
 export class AppModule {}
